ef4215e835159821bf9e0dcf19fd1705e5847875
watcharasak-na <43905161+watcharasak-na@users.noreply.github.com>
2018-11-07 01:37:44 +0700

    Update KKF-LineBot.php

--- KKF-LineBot.php
+++ KKF-LineBot.php
@@ -27,6 +27,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+}else if($arrJson['events'][0]['message']['text'] == "แผนวันนี้"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "OK รอแปป";
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
