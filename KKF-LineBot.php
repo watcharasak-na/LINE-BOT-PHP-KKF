5c935f3c169969deb75bbc15714321cf574ff4e6
watcharasak-na <43905161+watcharasak-na@users.noreply.github.com>
2018-11-07 01:44:06 +0700

    Update KKF-LineBot.php

--- KKF-LineBot.php
+++ KKF-LineBot.php
@@ -50,7 +50,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 if($arrJson['events'][0]['message']['text'] == "แผนวันนี้") {
-curl_setopt($ch, CURLOPT_URL, "https://script.google.com/d/1JOAv3B4BFK_iD96wwIjj2E8nKI4OI7pRft3u_Gpb5k4ZPK6aALFEUPg1/edit?usp=drive_web");
+curl_setopt($ch, CURLOPT_URL, "https://script.google.com/d/1JOAv3B4BFK_iD96wwIjj2E8nKI4OI7pRft3u_Gpb5k4ZPK6aALFEUPg1/edit?usp=drive_web/");
 }else{
 }
 $result = curl_exec($ch);
